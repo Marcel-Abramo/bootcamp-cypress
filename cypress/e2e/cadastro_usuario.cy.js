d8641ba76546c93c618fe3a0ac0f87490a5f1844
Marcel-Abramo <marcel.abramo.galego@gmail.com>
2025-06-19 18:13:21 -0300

    Added selectors list for login and register spec

--- cypress/e2e/cadastro_usuario.cy.js
+++ cypress/e2e/cadastro_usuario.cy.js
@@ -1,5 +1,16 @@
 describe('Cadastro Usuario', () =>{
 
+ const selectorsList={
+        cadastrarButton: '[data-testid="cadastrar"]',
+        nameField: '[data-testid="nome"]',
+        emailField: '[data-testid="email"]',
+        passwordField: '[data-testid="password"]',
+        checkBoxButton: '[data-testid="checkbox"]',
+        alert: '.alert',
+    }
+
+
+
     // Caso de teste 1
     it('Cadastrar novo usuario', () =>{
     
@@ -9,37 +20,37 @@ describe('Cadastro Usuario', () =>{
     const email = `roberto.jota${random}@gmail.com`;
 
     cy.visit('https://front.serverest.dev/login');
-    cy.get('[data-testid="cadastrar"]').click();
-    cy.get('[data-testid="nome"]').type(nome);
-    cy.get('[data-testid="email"]').type(email);
-    cy.get('[data-testid="password"]').type(123456);
-    cy.get('[data-testid="cadastrar"]').click();
+    cy.get(selectorsList.cadastrarButton).click();
+    cy.get(selectorsList.nameField).type(nome);
+    cy.get(selectorsList.emailField).type(email);
+    cy.get(selectorsList.passwordField).type(123456);
+    cy.get(selectorsList.cadastrarButton).click();
     cy.location('pathname').should('equal', '/cadastrarusuarios');
-    cy.get('.alert').contains('Cadastro realizado com sucesso');
+    cy.get(selectorsList.alert).contains('Cadastro realizado com sucesso');
     })
 
    // Caso de teste 2
     it('Cadastrar usuario invalido', () =>{
     cy.visit('https://front.serverest.dev/login');
-    cy.get('[data-testid="cadastrar"]').click();
-    cy.get('[data-testid="nome"]').type('Armando Golpes');
-    cy.get('[data-testid="email"]').type('armando@golpes');
-    cy.get('[data-testid="password"]').type(123456);
-    cy.get('[data-testid="cadastrar"]').click();
+    cy.get(selectorsList.cadastrarButton).click();
+    cy.get(selectorsList.nameField).type('Armando Golpes');
+    cy.get(selectorsList.emailField).type('armando@golpes');
+    cy.get(selectorsList.passwordField).type(123456);
+    cy.get(selectorsList.cadastrarButton).click();
     cy.location('pathname').should('equal', '/cadastrarusuarios');
-    cy.get('.alert').contains('Email deve ser um email válido');
+    cy.get(selectorsList.alert).contains('Email deve ser um email válido');
     })
 
     // Caso de teste 3
     it('Cadastrar usuario ja existente', () =>{
     cy.visit('https://front.serverest.dev/login');
-    cy.get('[data-testid="cadastrar"]').click();
-    cy.get('[data-testid="nome"]').type('Roberto Jota6');
-    cy.get('[data-testid="email"]').type('robertojota6@gmail.com');
-    cy.get('[data-testid="password"]').type(123456);
-    cy.get('[data-testid="cadastrar"]').click();
+    cy.get(selectorsList.cadastrarButton).click();
+    cy.get(selectorsList.nameField).type('Roberto Jota6');
+    cy.get(selectorsList.emailField).type('robertojota6@gmail.com');
+    cy.get(selectorsList.passwordField).type(123456);
+    cy.get(selectorsList.cadastrarButton).click();
     cy.location('pathname').should('equal', '/cadastrarusuarios');
-    cy.get('.alert').contains('Este email já está sendo usado');
+    cy.get(selectorsList.alert).contains('Este email já está sendo usado');
     })
 
     // Caso de teste 4
@@ -51,13 +62,13 @@ describe('Cadastro Usuario', () =>{
     const email = `marcos.ricardo${random}@gmail.com`;
 
     cy.visit('https://front.serverest.dev/login');
-    cy.get('[data-testid="cadastrar"]').click();
-    cy.get('[data-testid="nome"]').type(nome);
-    cy.get('[data-testid="email"]').type(email);
-    cy.get('[data-testid="password"]').type('123456');
-    cy.get('[data-testid="checkbox"]').click();
-    cy.get('[data-testid="cadastrar"]').click();
-    cy.get('.alert').contains('Cadastro realizado com sucesso');
+    cy.get(selectorsList.cadastrarButton).click();
+    cy.get(selectorsList.nameField).type(nome);
+    cy.get(selectorsList.emailField).type(email);
+    cy.get(selectorsList.passwordField).type('123456');
+    cy.get(selectorsList.checkBoxButton).click();
+    cy.get(selectorsList.cadastrarButton).click();
+    cy.get(selectorsList.alert).contains('Cadastro realizado com sucesso');
     cy.location('pathname').should('equal', '/admin/home');
     cy.get('.lead').contains('Este é seu sistema para administrar seu ecommerce.');
     })
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,21 @@
 describe('Login', () =>{
 
+    const selectorsList={
+        usernameField: '[data-testid="email"]',
+        passwordField: '[data-testid="senha"]',
+        loginButton: '[data-testid="entrar"]',
+    }
+
+
+
+
+
     // Caso de teste 1
     it('Login com sucesso', () => {
     cy.visit('https://front.serverest.dev/login');
-    cy.get('[data-testid="email"]').type('robertojota6@gmail.com');
-    cy.get('[data-testid="senha"]').type('123456');
-    cy.get('[data-testid="entrar"]').click();
+    cy.get(selectorsList.usernameField).type('robertojota6@gmail.com');
+    cy.get(selectorsList.passwordField).type('123456');
+    cy.get(selectorsList.loginButton).click();
     cy.location('pathname').should('equal', '/home');
     cy.get('h1').contains('Serverest Store');
     })
@@ -13,25 +23,25 @@ describe('Login', () =>{
     // Caso de teste 2
     it('Email invalido', () =>{
     cy.visit('https://front.serverest.dev/login');
-    cy.get('[data-testid="email"]').type('asdasd@asasdasd');
-    cy.get('[data-testid="senha"]').type('089867');
-    cy.get('[data-testid="entrar"]').click();
+    cy.get(selectorsList.usernameField).type('asdasd@asasdasd');
+    cy.get(selectorsList.passwordField).type('089867');
+    cy.get(selectorsList.loginButton).click();
     cy.get('.alert').contains('Email deve ser um email válido');
     })
 
     // Caso de teste 3
     it('Senha invalida', () =>{
     cy.visit('https://front.serverest.dev/login');
-    cy.get('[data-testid="email"]').type('robertojota6@gmail.com');
-    cy.get('[data-testid="senha"]').type('089867');
-    cy.get('[data-testid="entrar"]').click();
+    cy.get(selectorsList.usernameField).type('robertojota6@gmail.com');
+    cy.get(selectorsList.passwordField).type('089867');
+    cy.get(selectorsList.loginButton).click();
     cy.get('.alert-secondary').contains('Email e/ou senha inválidos');
     })
      
     // Caso de teste 4
     it('Logar com campo usuário e senha em branco', () =>{
     cy.visit('https://front.serverest.dev/login');
-    cy.get('[data-testid="entrar"]').click();
+    cy.get(selectorsList.loginButton).click();
     cy.get('.form > :nth-child(3)').contains('Email é obrigatório')
     cy.get('.form > :nth-child(4)').contains('Password é obrigatório')
     })
